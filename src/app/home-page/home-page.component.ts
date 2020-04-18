8ae4046ca106c4bdc55b044d3861fd3636df16fa
flamant <flamant@club-internet.fr>
2020-04-18 10:19:42 +0200

    “clean”

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,9 +1,8 @@
 import { Component, OnInit, AfterViewInit } from '@angular/core';
-import { FormBuilder, FormGroup, FormControl, Validators, AbstractControl, ValidatorFn } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CommonService } from '../common.service';
 import { ProduitImmobilierDTO } from '../produit-immobilier-dto';
 
-declare var $: any;
 
 @Component({
   selector: 'app-home-page',
@@ -19,7 +18,7 @@ export class HomePageComponent implements OnInit, AfterViewInit {
   // tslint:disable-next-line: max-line-length
   public annonce: ProduitImmobilierDTO = new ProduitImmobilierDTO(0, 'Flat', '0622136822', 'Pug flexitarian viral cliche pour-over. Banjo tote bag vinyl, lumbersexual 8-bit fam +1 chicharrones disrupt artisan vice activated charcoal offal succulents. Cornhole lyft williamsburg waistcoat put a bird on it banjo sriracha iceland snackwave keffiyeh kinfolk. Bespoke drinking vinegar mixtape hexagon DIY viral kitsch ramps poutine four dollar toast food truck ethical venmo keffiyeh.', 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 'S', 'A', 1, 1, 2, 1, 1, 1, 1, 1, 1, '10', 1, 1, '18th district', '75018', 'Paris', 'A', 690000, 190, 1, 1, 1, 1, 1, 1, 1, 'V', 1);
 
-  private color: string = 'white';
+  public color = 'white';
 
   constructor(private formBuilder: FormBuilder, private commonService: CommonService) { }
 
@@ -35,24 +34,18 @@ export class HomePageComponent implements OnInit, AfterViewInit {
     this.searchFormGroup = this.formBuilder.group({
         ville: ['', Validators.required],
         prixMax: ['', Validators.required]
-    });            
+    });
     this.commonService.currentColor.subscribe(color => this.color = color);
     this.newColor('white');
-    //this.investmentFormGroup.get('liveRentChoice').setValue('LiveIn');
   }
 
   ngAfterViewInit() {
-    // console.log('ngAfterViewInit');
-    // console.log($("#ext1"));
-    // $("#ext1").slider({
-    //   tooltip: 'always'
-    // });
-    const allRanges = document.querySelectorAll(".range-wrap");
+    const allRanges = document.querySelectorAll('.range-wrap');
     allRanges.forEach(wrap => {
-      const range = wrap.querySelector(".range");
-      const bubble = wrap.querySelector(".bubble");
+      const range = wrap.querySelector('.range');
+      const bubble = wrap.querySelector('.bubble');
       const that = this;
-      range.addEventListener("input", () => {
+      range.addEventListener('input', () => {
         that.setBubble(range, bubble);
       });
       that.setBubble(range, bubble);
@@ -69,8 +62,8 @@ export class HomePageComponent implements OnInit, AfterViewInit {
     // Sorta magic numbers based on size of the native UI thumb
     bubble.style.left = `calc(${newVal}% + (${8 - newVal * 0.15}px))`;
   }
-  
-  
+
+
   newColor(color: string) {
     this.commonService.changeColor(color);
   }
